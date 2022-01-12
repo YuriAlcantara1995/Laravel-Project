0580e356abe2a10d402d93c06d25ca1c5e82a335
Yuri Alcantara <yuri.alcantara1995@gmail.com>
2022-01-12 03:49:20 -0500

    Authentication added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', \App\Http\Controllers\WelcomeController::class);
+Route::get('/', App\Http\Controllers\WelcomeController::class);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
