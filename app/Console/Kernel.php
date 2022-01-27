267ab89c9c0eddbddc1a2b42ab7ec3d71668fb04
Yuri Alcantara <yuri.alcantara1995@gmail.com>
2022-01-27 17:59:12 -0500

    Scheduled daily backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(\Spatie\Health\Commands\RunHealthChecksCommand::class)->everyMinute();
+        $schedule->command('make:backup')->daily()->at('04:00');
     }
 
     /**
